a769d6859fa96552169251e2e7e4e60a8d1acd88
Jeffrey C <Saporous@users.noreply.github.com>
2022-10-19 19:59:40 -0700

    initial commit

---
+++ chapter_1/summary.py
@@ -0,0 +1,5 @@
+# Automating the Boring Stuff
+# ----------------------------
+# Chapter 1 Summary Program
+# Objective: Summarize the contents of the chapter in a simple text program
+print('Chapter 1')
