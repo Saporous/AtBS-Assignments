5d091a1d13d6b45681611dcb47da62d1e41ba185
Jeffrey C <Saporous@users.noreply.github.com>
2022-10-26 21:42:50 -0700

    add chapter 3 summary

--- chapter_3/summary.py
+++ chapter_3/summary.py
@@ -4,7 +4,7 @@
 # Objective: Summarize the contents of the chapter in a
 # simple and interactive text program
 
-def myFunc(arg1, [arg2 = 0]):
+def myFunc(arg1, arg2 = 0):
     print(arg1)
     print(arg2)
 
@@ -18,10 +18,11 @@ def myFunc(arg1, [arg2 = 0]):
 # A *return* statement consists of the *return* keyword and a value or expression to return
 
 # The None Value
+print('\nThe None Value')
 # *None* is a unique reserved keyword with type NoneType
 # *None* is the default return value for functions without a return statement
 blah = print("asdf")
-print("None == blah: " + str(None== blah))
+print("None == blah: " + str(None == blah))
 
 # print() Keyword Arguments 
 # end='\n'
@@ -30,3 +31,30 @@ print('World')
 # sep = ''
 print('a', 'b', 'c', sep=',')
 
+# Local and Global Scope
+print('\nLocal and Global Scope')
+# Global variables can be read from local scope as long as it is defined before execution
+blah2 = "blah2"
+def modBlah():
+    print(blah2)
+
+modBlah()
+
+def globBlah():
+    global blah2
+    blah2 = "modBlah"
+
+print(blah2)
+
+# Exception handling
+print('\nException Handling')
+# Programs default to the *try:* code block. If an error occurs it immediately begins
+# execution of the *except:* code block
+try:
+    print(42/21)
+    print(42/0)
+    print(42/42)
+except ZeroDivisionError:
+    print("Error: Invalid argument")
+
+print("Exception complete")
