d51a3cb4adc1c33705224825b802ebf1cd62bb9e
Jeffrey C <Saporous@users.noreply.github.com>
2022-11-04 00:26:06 -0700

    finish program 1

--- chapter_5/chess_dictionary_validator.py
+++ chapter_5/chess_dictionary_validator.py
@@ -1,8 +1,11 @@
 board = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
+pieceDict = {'pawn', 'knight', 'bishop', 'rook', 'queen', 'king'}
 
 def isValidChessBoard(chessBoard):
-    numPawns = 0
-    numPieces = 0
+    numPawnsW = 0
+    numPawnsB = 0
+    numPiecesW = 0
+    numPiecesB = 0
 
     if ('wking' not in chessBoard.values()) or ('bking' not in chessBoard.values()):
         print('Missing King!')
@@ -12,7 +15,28 @@ def isValidChessBoard(chessBoard):
         if position[0] > '8' or position[0] < '1' or position[1] > 'h' or position[1] < 'a':
             print(piece + ' is in invalid position ' + position)
             return False
-
+        if piece[0] == 'w':
+            numPiecesW += 1
+        elif piece[0] == 'b':
+            numPiecesB += 1
+        else:
+            print('Missing black/white indication: ' + piece)
+            return False
+        if piece[1:] not in pieceDict:
+            print('Invalid piece: ' + piece[1:])
+            return False
+    if numPawnsW > 8:
+        print('Too many pawns: ' + str(numPawnsW))
+        return False
+    if numPawnsB > 8:
+        print('Too many pawns: ' + str(numPawnsB))
+        return False
+    if numPiecesW > 16:
+        print('Too many pieces: ' + str(numPiecesW))
+        return False
+    if numPiecesB > 16:
+        print('Too many pieces: ' + str(numPiecesB))
+        return False
     return True
 
-isValidChessBoard(board)
+print(str(isValidChessBoard(board)))
